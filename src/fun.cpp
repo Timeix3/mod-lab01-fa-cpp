d3c0a87f44f717b9ff4b30d4dfb5264ee2096069
Timeix3 <timeix3@gmail.com>
2025-03-13 23:46:25 +0300

    added 3 functions

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,61 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str) {
+    int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    while (*str) {
+        if (*str != ' ' && inWord == false) {
+            hasDigit = false;
+            if (isdigit(*str)) hasDigit = true;
+            inWord = true;
+        } else if (inWord == true && isdigit(*str)) {
+            hasDigit = true;
+        } else if (*str == ' ' && inWord == true) {
+            inWord = false;
+            if (!hasDigit) count++;
+        }
+        str++;
+    }
+    return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    int count = 0;
+    bool inWord = false;
+    bool rightFormat = true;
+    while (*str) {
+        if (*str != ' ' && inWord == false) {
+            rightFormat = false;
+            if (isupper(*str)) rightFormat = true;
+            inWord = true;
+        } else if (inWord == true && *str != ' ' && !islower(*str)) {
+            rightFormat = false;
+        } else if (*str == ' ' && inWord == true) {
+            inWord = false;
+            if (rightFormat) count++;
+        }
+        str++;
+    }
+    return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    int count = 0;
+    double sum = 0;
+    bool inWord = false;
+    while (*str) {
+        if (*str != ' ' && inWord == false) {
+            count++;
+            sum++;
+            inWord = true;
+        } else if (*str != ' ' && inWord == true) {
+            sum++;
+        } else if (*str == ' ' && inWord == true) {
+            inWord = false;
+        }
+        str++;
+    }
+    return round(sum/count);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,7 @@
 #include "fun.h"
 
 int main() {
-    return 0;
+    std::cout << faStr1("f?umKD7' !5i5g8<M  y;?oDq%H") << std::endl;
+    std::cout << faStr2("KCbDbHaomKdO GJXXe  Zxxjptc") << std::endl;
+    std::cout << faStr3("pSgWeFV9 mVI2M YB7glKoltrWmad") << std::endl;
 }
